ca805ac1d739d8fc476bafdd317f907b1c73f0db
Andreea Muscalagiu <m_andreea93@yahoo.com>
2016-06-21 19:12:00 +0200

    test: add tests

--- find_reg_ex.py
+++ find_reg_ex.py
@@ -1,17 +1,13 @@
 import re
-import glob
 
 def find(pattern, files):
 	ids = []
-	for file in files:
-		lines = open(file, "r" ).readlines()
+	for fileName in files:
+		file = open(fileName, "r" )
+		lines = file.readlines()
 		for line in lines:
 			res = re.search(pattern, line, re.DOTALL)
 			if res is not None:
 				ids.append(re.search('^(\d+)', line[:res.start()]).group(0))
+		file.close()
 	return ids
-
-pattern = 'Section \d+ Refresh'
-files = glob.glob('./*.html')
-ids = find(pattern, files)
-print(ids)
