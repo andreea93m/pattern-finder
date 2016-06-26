e8d44248cf8d745b80e420c2727b8b73d44bffcf
Andreea Muscalagiu <m_andreea93@yahoo.com>
2016-06-26 12:19:48 +0200

    feat: add pool of processes for searching in files

--- find_reg_ex.py
+++ find_reg_ex.py
@@ -1,8 +1,21 @@
 import re
+import itertools
+from multiprocessing import Pool
 
-def find(pattern, files):
-	ids = []
-	for fileName in files:
+class PatternFinder(object):
+
+	def __init__(self):
+		super(PatternFinder, self).__init__()
+		self.pool = Pool(processes=4)
+
+	def find(self, pattern, files):
+		ids = []
+		res = self.pool.starmap(self.find_in_file, zip([pattern]*len(files), files))
+		ids = list(itertools.chain.from_iterable(res))
+		return ids
+
+	def find_in_file(self, pattern, fileName):
+		ids = []
 		file = open(fileName, "r" )
 		lines = file.readlines()
 		for line in lines:
@@ -10,4 +23,12 @@ def find(pattern, files):
 			if res is not None:
 				ids.append(re.search('^(\d+)', line[:res.start()]).group(0))
 		file.close()
-	return ids
+		return ids
+
+	def __getstate__(self):
+		self_dict = self.__dict__.copy()
+		del self_dict['pool']
+		return self_dict
+
+	def __setstate__(self, state):
+		self.__dict__.update(state)
